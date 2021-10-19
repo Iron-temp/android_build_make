LineageOS/android_build

build: Conditionally include AudioPackage14.mk instead of AllAudio.mk
* AllAudio.mk doesn't contain latest material files and also
  contains multiple duplicate files. Just copy the latest
  material ones and we'll include the older files in vendor/lineage

Change-Id: Ic7353216a3f3dc3c51006d951ac932cfd6e5f3b5

target/product/full_base.mk
@@ -50,7 +50,11 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
+ifeq ($(LINEAGE_BUILD),)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+else
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
+endif
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)

Iron-temp/android_build_make

target/product/full_base.mk
@@ -51,7 +51,11 @@ PRODUCT_VENDOR_PROPERTIES := \
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
+ifeq ($(ORGASMIC_BUILD),)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+else
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
+endif
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)