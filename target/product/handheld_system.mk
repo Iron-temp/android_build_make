58ead271467cdc5a85a4420eaa6046d336e6717c
Bicet <bicets@gmail.com>
2021-10-20 09:11:32 +0200

    Goodbye Spark, welcome Orgasmic

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -85,7 +85,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_VENDOR_PROPERTIES += \
     ro.carrier?=unknown \
 
-ifeq ($(SPARK_BUILD),)
+ifeq ($(ORGASMIC_BUILD),)
 PRODUCT_VENDOR_PROPERTIES += \
     ro.config.notification_sound?=OnTheHunt.ogg \
     ro.config.alarm_alert?=Alarm_Classic.ogg
